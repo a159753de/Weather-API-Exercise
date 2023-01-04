fb57cb7c7d6ace1109a513b3a2c60f3b042e03fb
Andrew <g6961116@gmail.com>
2023-01-04 22:05:10 +0800

    find data

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const apikey = "CWB-6A211101-534C-445A-BB3A-5D7B61E126A4";
 const url = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${apikey}&format=JSON`;
 fetch(url)
   .then((response) => {
-    const data = response.json();
-    return data;
+    return response.json();
   })
   .then((response) => {
+    //全部資料
     console.log(response);
+    //location
+    const dataLocation = response.records.location;
+    const dataWeatherElement = response.records.WeatherElement;
+    console.log(dataLocation);
+    //獲取資料
+    let location = [];
+    let weatherElement = [];
+    let Wx = [];
+    dataLocation.forEach((ele) => {
+      location.push(ele.locationName);
+      weatherElement.push(ele.weatherElement);
+    });
+    console.log(location);
+    console.log(weatherElement);
   })
   .catch((err) => {
     console.log(err);
