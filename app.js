4883a8e5233e3ec80dff697d760863190ed84b92
Andrew <g6961116@gmail.com>
2023-02-07 15:52:23 +0800

    Successfully use the data'

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ fetch(url)
   })
   .then((response) => {
     //全部資料
+    console.log("全部資料");
     console.log(response);
     //location
     const dataLocation = response.records.location;
     const dataWeatherElement = response.records.WeatherElement;
+    console.log("location");
     console.log(dataLocation);
     //獲取資料
     let location = [];
@@ -19,8 +21,24 @@ fetch(url)
       location.push(ele.locationName);
       weatherElement.push(ele.weatherElement);
     });
+    console.log("location name");
     console.log(location);
+    console.log("氣象資料(很多種喔)");
     console.log(weatherElement);
+
+    const nameLength = location.length;
+
+    for (let i = 0; i < nameLength; i++) {
+      const div = document.createElement("div");
+      div.setAttribute("class", location[i]);
+      div.innerHTML = `<h2>${location[i]}</h2> <br />
+      天氣：<br />
+      ${weatherElement[i][0].time[0].parameter.parameterName}`;
+      // const localName = document.createElement("h3");
+      // localName.textContent = location[i];
+      // document.querySelector("div").appendChild(localName);
+      document.querySelector("section").appendChild(div);
+    }
   })
   .catch((err) => {
     console.log(err);
